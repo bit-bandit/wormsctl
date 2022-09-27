146f5fce8fcbda0d301ba168e55083bfef51ec43
zegfarce <zegfarce@protonmail.com>
2022-09-27 01:00:47 -0500

    Rewrote it to work less hardcodedly and use less punctuation

--- database_utils.ts
+++ database_utils.ts
@@ -6,58 +6,115 @@ const db_settings = instanceSettings.database.settings;
 const client = new Client(db_settings);
 
 export async function basicSelect(
-  row: string,
+  column: string,
   table: string,
-  name: string,
+  id: string,
 ) {
-  await client.connect();
-  const res = await client.queryArray(
-    `SELECT ${row} FROM ${table} WHERE id = $1`,
-    [name],
-  );
-  await client.end();
+  let res;
+
+  try {
+    await client.connect();
+    res = await client.queryObject(
+      `SELECT ${column} FROM ${table} WHERE id = $1`,
+      [id],
+    );
+    await client.end();
+  } catch (e) {
+    return { "err": true, "msg": e.toString() };
+  }
 
   if (res.rows.length !== 0) {
     return res.rows[0];
   }
 
-  return { "err": true, "msg": msg };
+  return {
+    "err": true,
+    "msg": `row of id '${id}' in table '${table}' doesn't exist`,
+  };
 }
 
 export async function basicUpdate(
-  category: string,
+  table: string,
   params = {},
   id: string,
 ) {
-  await client.connect();
-
-  for (const prop in params) {
-    await client.queryArray(
-      `UPDATE ${category} SET ${prop} = $1 WHERE id = $2`,
-      [JSON.stringify(params[prop]), id],
-    );
+  try {
+    await client.connect();
+    for (const column in params) {
+      await client.queryObject(
+        `UPDATE ${table} SET ${column} = $1 WHERE id = $2`,
+        [JSON.stringify(params[column]), id],
+      );
+    }
+    await client.end();
+  } catch (e) {
+    return { "err": true, "msg": e.toString() };
   }
-
-  await client.end();
 }
 
 export async function basicDelete(
   table: string,
-  name: string,
+  id: string,
 ) {
-  await client.connect();
-  await client.queryArray(
-    `DELETE FROM ${table} WHERE id = $1`,
-    [name],
-  );
-  if (table === "users") {
-    const defaultTables = ["comments", "torrents", "lists", "actions"];
-    for await (const defaultTable of defaultTables) {
-      await client.queryArray(
-        `DELETE FROM ${defaultTable} WHERE uploader = $1`,
-        [id],
-      );
+  try {
+    await client.connect();
+    await client.queryArray(
+      `DELETE FROM ${table} WHERE id = $1`,
+      [id],
+    );
+
+    console.log(`Deleted row of id '${id}' from table '${table}'.`);
+
+    if (table === "users") {
+      const defaultTables = ["comments", "torrents", "lists", "actions"];
+      for await (const defaultTable of defaultTables) {
+        await client.queryObject(
+          `DELETE FROM ${defaultTable} WHERE uploader = $1`,
+          [id],
+        );
+      }
+    }
+    await client.end();
+  } catch (e) {
+    return { "err": true, "msg": e.toString() };
+  }
+}
+
+export async function basicExec() {
+  try {
+    return await client.queryObject(statement);
+  } catch (e) {
+    return { "err": true, "msg": e.toString() };
+  }
+}
+
+const blacklist = ["command", "query"];
+
+export async function basicShell() {
+  console.log("A basic shell into your db server.");
+  console.log("Send an empty statement to exit.");
+  console.log("");
+
+  while (true) {
+    const statement = prompt("=>");
+
+    if (!statement) {
+      return;
+    }
+
+    try {
+      const res = await client.queryObject(statement);
+      const disp = Object.assign({}, res);
+
+      for (const key of blacklist) {
+        if (key in disp) {
+          delete disp[key];
+        }
+      }
+
+      console.log(disp);
+    } catch (e) {
+      console.log(e);
     }
   }
-  await client.end();
 }
--- wormsctl.ts
+++ wormsctl.ts
@@ -1,89 +1,207 @@
-// Flag parser that doesn't make me want to cut my fingers off.
-import { parse } from "https://deno.land/std/flags/mod.ts";
+#!/usr/bin/env -S deno run --allow-env --allow-net
 
 // File to share between wormscli and your parasite instance.
 // We recommend you do this locally.
 import instances from "../parasite/federation.json" assert { type: "json" };
 import { roles } from "../parasite/roles.ts";
 
-import { basicDelete, basicSelect, basicUpdate } from "./database_utils.ts";
+import * as dbutils from "./database_utils.ts";
 
-const flags = parse(Deno.args);
+function printCommands() {
+  console.log("Commands:");
+  console.log("  get table id [column]");
+  console.log("  get table.id[.column]:");
+  console.log("    Returns data for a row by id in a table.");
+  console.log("    If a column is given, returns that column.");
+  console.log("  set table id column to value");
+  console.log("  set table.id.column to value:");
+  console.log("    Sets a column of a row by id in a table, to value.");
+  console.log("  delete table id");
+  console.log("  delete table.id:");
+  console.log("    Deletes a row by id into table.");
+  console.log("    This also deletes user data if a user is deleted.");
+}
 
-flags._ = flags._[0];
+function printUsage() {
+  console.log("Usage: wormsctl [command args...]");
+  console.log("Open wormsctl without arguments for a shell.");
+  console.log("");
+  printCommands();
+}
 
-function handleOut(res: any) { // Too lazy to assign an actual interface/type to this
+// Too lazy to assign an actual interface/type to this
+function handleOut(res: unknown) {
   if (!res.err) {
-    console.log(res[0]);
+    console.log(res);
   } else {
     console.log(`ERROR: ${res.msg}`);
   }
 }
 
-if (flags.showArgs) {
-  console.log(flags);
+// Required to terminate identifiers
+function isToOrEquals(str) {
+  return str.toLowerCase() === "to" || str === "=";
 }
-switch (flags._) {
-  case "remove": {
-    if (flags.torrent) {
-      basicDelete("torrents", flags.torrent);
-    }
-    if (flags.list) {
-      basicDelete("lists", flags.list);
-    }
-    if (flags.comment) {
-      basicDelete("comments", flags.comment);
-    }
-    if (flags.user) {
-      // This will have consequences. You have been warned.
-      basicDelete("users", flags.user);
-    }
-    break;
-  }
-  case "set": {
-    if (flags.role) {
-      if (!flags.user) {
-        console.log(`ERROR: 'user' flag must be present!`);
+
+// Reads a table, row, and column that are optionally seperated by .
+function getIdentifier(args) {
+  const names = ["table", "id", "column"];
+  const identifier = {};
+
+  while (args.length && names.length) {
+    const read = args.shift().split(".");
+
+    while (read.length && names.length) {
+      const arg = read.shift();
+
+      if (isToOrEquals(arg)) {
+        break;
       }
-      await basicUpdate("users", {
-        "roles": roles[flags.role],
-      }, flags.user);
+
+      identifier[names.shift()] = arg;
     }
-    break;
   }
-  case "show": {
-    // What else can I put here?
-    if (flags.role) {
-      if (!flags.user) {
-        console.log(`ERROR: 'user' flag must be present!`);
+
+  // If we didn't read enough (need at least table.column)
+  if (names.length > 1) {
+    return null;
+  }
+
+  return identifier;
+}
+
+// The function that actually does things
+async function execute(statement) {
+  // Replit by both . and ' '
+  const args = statement.split(" ");
+  const command = args.shift();
+
+  switch (command) {
+    case "get": {
+      const info = getIdentifier(args);
+
+      if (!info) {
+        return console.log(
+          `ERROR: identifier must have table, id, and optionally column.`,
+        );
       }
-      const res = await basicSelect("roles", "users", flags.user);
+
+      if (args.length) {
+        return console.log(
+          `ERROR: too many arguments for command '${command}'.`,
+        );
+      }
+
+      const res = await dbutils.basicSelect(
+        info.column ?? "*",
+        info.table,
+        info.id,
+      );
+
       handleOut(res);
+
+      break;
     }
-    if (flags.json) {
-      if (flags.torrent) {
-        const res = await basicSelect("json", "torrents", flags.torrent);
-        handleOut(res);
+    case "set": {
+      const info = getIdentifier(args);
+
+      if (!info || !("column" in info)) {
+        return console.log(
+          `ERROR: identifier must have table, id, and column.`,
+        );
       }
-      if (flags.list) {
-        const res = await basicSelect("json", "lists", flags.list);
-        handleOut(res);
+
+      if (!args.length || !isToOrEquals(args.shift())) {
+        return console.log(
+          `ERROR: '${command}' command requires 'to' or '=' after identifier.`,
+        );
       }
-      if (flags.comment) {
-        const res = await basicSelect("json", "comments", flags.comment);
-        handleOut(res);
+
+      if (!args.length) {
+        return console.log(
+          `ERROR: '${command}' command requires a value after the 'to' or '='.`,
+        );
       }
+
+      const value = args.shift();
+
+      if (args.length) {
+        return console.log(
+          `ERROR: too many arguments for command '${command}'.`,
+        );
+      }
+
+      const res = await dbutils.basicUpdate(info.table, {
+        [info.column]: value,
+      }, info.id);
+
+      break;
     }
-    break;
+    case "remove":
+    case "delete": {
+      const info = getIdentifier(args);
+
+      if (!info || "column" in info) {
+        return console.log(`ERROR: identifier must only have table and id.`);
+      }
+
+      if (args.length) {
+        return console.log(
+          `ERROR: too many arguments for command '${command}'.`,
+        );
+      }
+
+      const res = await dbutils.basicDelete(info.table, info.id);
+
+      break;
+    }
+    case "shell": {
+      if (args.length) {
+        return handleOut(await dbutils.basicExec(args.join(" ")));
+      } else {
+        await dbutils.basicShell();
+      }
+
+      break;
+    }
+    case "help": {
+      printCommands();
+      break;
+    }
+    default: {
+      console.log(`ERROR: Invalid command '${command}'.`);
+      console.log();
+
+      printCommands();
+    }
   }
-  case (undefined):
-  case ("help"): {
-    console.log(
-      `usage: wormsctl (show,set,remove) --json --role --user ---list --torrent --comment`,
-    );
-    break;
+}
+
+// the stanky part of the code that handles arguments
+// and also the part of the code that does the main repl
+if (Deno.args.length) {
+  if (
+    Deno.args.includes("-h") ||
+    Deno.args.includes("-help") ||
+    Deno.args.includes("--help")
+  ) {
+    printUsage();
+  } else {
+    await execute(Deno.args.join(" "));
   }
-  default: {
-    console.log(`ERROR: '${flags._}' is not a valid argument!`);
+} else {
+  console.log("wormsctl shell");
+  console.log("Send an empty statement to exit.");
+  console.log("Type 'help' for a list of commands.");
+  console.log("");
+
+  while (true) {
+    const commands = prompt(">");
+
+    if (!commands || !commands.length) {
+      break;
+    }
+
+    await execute(commands);
   }
 }
